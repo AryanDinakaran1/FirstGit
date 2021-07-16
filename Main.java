b29612683f6eef89bd8fd971364744ee583bc39b
AryanDinakaran1 <aryandinakaran@gmail.com>
2021-07-16 17:46:43 +0000

    Added Sorting using Bubble Sort

--- Main.java
+++ Main.java
@@ -3,7 +3,20 @@ public class Main
 	//Bubble Sort
 	public static void BubbleSort(int array[])
 	{
+		int len = array.length;
 
+		for(int i = 0; i < len - 1; i++)
+		{
+			for(int j = 0; j < len - i - 1;j++)
+			{
+				if(array[j] > array[j+1])
+				{
+					int temp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = temp;
+				}
+			}
+		}
 	}
 	
 	//Linear Search
@@ -25,7 +38,8 @@ public class Main
 	public static void main(String args[])
 	{
 		int[] array = {4,9,0,3,5,6,1,4};
-		
+
+		BubbleSort(array);		
 		LinearSearch(array,4);
 
 	}
