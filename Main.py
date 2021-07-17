c748d5cc572a4562d575aec74a65e9cccf8c3744
AryanDinakaran1 <aryandinakaran@gmail.com>
2021-07-17 10:51:25 +0000

    Catching errors

--- Main.py
+++ Main.py
@@ -1,7 +1,10 @@
-num1 = int(input("Enter First Number\n>> "))
-num2 = int(input("Enter Last Number\n>> "))
+try:
+    num1 = int(input("Enter First Number\n>> "))
+    num2 = int(input("Enter Last Number\n>> "))
 
-ans = num1+num2
+    ans = num1+num2
 
-print(ans)
-print("Aryan is a Linux Hater")
+    print(ans)
+    print("Aryan is a Linux Hater")
+except:
+    print("Something went wrong")
