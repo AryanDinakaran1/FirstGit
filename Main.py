bd3a97f9d22c8bcc32944b8229b00fb0b735d15a
AppDesign1 <appdesign33@gmail.com>
2021-07-17 20:30:27 +0530

    Original Program

--- Main.py
+++ Main.py
@@ -1,10 +1,5 @@
-try:
-    num1 = int(input("Enter First Number\n>> "))
-    num2 = int(input("Enter Last Number\n>> "))
+num1 = int(input("Enter First Number\n>> "))
+num2 = int(input("Enter Last Number\n>> "))
 
-    ans = num1+num2
-
-    print(ans)
-    print("Aryan is a Linux Hater")
-except:
-    print("Something went wrong")
+ans = num1+num2
+print(ans)
